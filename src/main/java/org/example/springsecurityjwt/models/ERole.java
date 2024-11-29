5eb6622bd463b7963c3e631e7ec4443a3256a3b1
wahib-git <wahibbachoua95@gmail.com>
2024-11-29 17:13:50 +0100

    Initial commit

---
+++ src/main/java/org/example/springsecurityjwt/models/ERole.java
@@ -0,0 +1,7 @@
+package org.example.springsecurityjwt.models;
+
+public enum ERole {
+    ROLE_USER,
+    ROLE_MODERATOR,
+    ROLE_ADMIN
+}
---
+++ src/main/java/org/example/springsecurityjwt/repositories/RoleRepository.java
@@ -0,0 +1,10 @@
+package org.example.springsecurityjwt.repositories;
+import org.example.springsecurityjwt.models.ERole;
+import org.example.springsecurityjwt.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository <Role, Long> {
+    Optional<Role> findByName(ERole name);
+}
